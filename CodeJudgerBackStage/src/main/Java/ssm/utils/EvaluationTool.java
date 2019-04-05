8013a20d3f7d5857e7e77002467e66f33a6e73d5
caozixuan <1240292104@qq.com>
2019-04-05 20:30:17 +0800

    judger interface

---
+++ CodeJudgerBackStage/src/main/Java/ssm/utils/EvaluationTool.java
@@ -0,0 +1,30 @@
+package ssm.utils;
+
+import ssm.dao.IProblemDao;
+import ssm.model.Problem;
+import ssm.model.Submit;
+
+import javax.annotation.Resource;
+
+public class EvaluationTool{
+
+    @Resource
+    private IProblemDao iProblemDao;
+
+    private Compiler compiler = new Compiler();
+
+    private Judger judger = new Judger();
+
+    public boolean evaluate (Submit submit)throws Exception
+    {
+        String problemID = submit.getProblemID();
+        Problem problem = iProblemDao.getProblemByID(problemID);
+        if(!compiler.compileCode(submit))
+        {
+            return false;
+        }
+        if(!judger.run(submit))
+            return false;
+        return true;
+    }
+}
